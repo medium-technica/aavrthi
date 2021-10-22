8dd86129a9738b405db73ffd1ce040960bb9b5c9
Abraham <abey.alexander@gmail.com>
2021-10-22 14:27:14 +0530

    update

--- static/aavarthi.js
+++ static/aavarthi.js
@@ -2,7 +2,6 @@ $(fnInit())
 
 var ListFiles = [];
 var Articles;
-var indexFile;
 
 function fnInit() {
 	console.log("init");
@@ -10,16 +9,15 @@ function fnInit() {
 }
 
 function loadFilesList() {
-	var i = 0;
 	fetch("static/res/doc/text.json")
 		.then(response => response.json())
 		.then(data => {
 			//console.log(data);
 			Articles = data;
+			i = 0;
 			$.each(data, function (key, data) {
 				ListFiles.push(key);
 				$('#ListMenu').append(`<a href="#top" onclick="loadFile('` + key + `',` + i + `)" class=" mdl-navigation__link ">` + key + `</a>`);
-				//$('#ListMenu').append(`<a href="?a=` + i + `" class=" mdl-navigation__link ">` + key + `</a>`);
 				i++;
 			})
 			const urlSearchParams = new URLSearchParams(window.location.search);
@@ -37,6 +35,9 @@ function loadFilesList() {
 
 function loadPrev() {
 	console.log("Prev");
+	const urlSearchParams = new URLSearchParams(window.location.search);
+	const params = Object.fromEntries(urlSearchParams.entries());
+	indexFile = parseInt(params["a"]);
 	if (indexFile > 0)
 		indexFile--;
 	if (ListFiles.length > 0) {
@@ -47,6 +48,9 @@ function loadPrev() {
 
 function loadNext() {
 	console.log("Next");
+	const urlSearchParams = new URLSearchParams(window.location.search);
+	const params = Object.fromEntries(urlSearchParams.entries());
+	indexFile = parseInt(params["a"]);
 	if (indexFile < ListFiles.length - 1)
 		indexFile++;
 	if (ListFiles.length > 0) {
@@ -55,23 +59,23 @@ function loadNext() {
 	}
 }
 
-function loadFile(nameFile, i, event) {
+function loadFile(nameFile, index, event) {
 	if (event) {
-		//event.preventDefault();
+		event.preventDefault();
 	}
-	//console.log(nameFile);
+	console.log(nameFile, index);
 	$('.mdl-layout__drawer').attr("class", "mdl-layout__drawer");
 	$('.mdl-layout__obfuscator').attr("class", "mdl-layout__obfuscator");
 	$('.mdl-layout-title').html(nameFile);
 	$('title').html(nameFile);
 	$('.page-content').html(Articles[nameFile]);
-	history.pushState({}, null, "/aavarthi/?a=" + i);
-	if (indexFile == 0) {
+	history.pushState({}, null, "/aavarthi/?a=" + index);
+	if (index == 0) {
 		$('.mdl-paging__prev').prop("hidden", true);
 	} else {
 		$('.mdl-paging__prev').prop("hidden", false);
 	}
-	if (indexFile == ListFiles.length - 1) {
+	if (index == ListFiles.length - 1) {
 		$('.mdl-paging__next').prop("hidden", true);
 	} else {
 		$('.mdl-paging__next').prop("hidden", false);
