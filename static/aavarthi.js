d36bbeb0715a43b8843891442937d81895863d2b
abraham <55025669+medium-technica@users.noreply.github.com>
2023-01-19 13:00:32 +0530

    Update aavarthi.js

--- static/aavarthi.js
+++ static/aavarthi.js
@@ -21,8 +21,8 @@ function fnLoadData() {
     indexArray++;
     ListFiles.push(value);
    })
-   titleFileURL = decodeURIComponent(getURLParam('t'));
-   //console.log(Articles[titleFileURL]);
+   titleFileURL = decodeURIComponent(getURLParam('t')).replace(/_/g, " ");
+   //console.log(titleFileURL, Articles[titleFileURL]);
    indexFile = Object.keys(Articles).indexOf(titleFileURL);
    console.log("index from Name:" + indexFile);
    if (indexFile >= 0 && ListFiles.length > 0) {
@@ -36,7 +36,8 @@ function fnLoadData() {
 
 function loadPrev() {
  console.log("Prev");
- titleFileURL = decodeURIComponent(getURLParam('t'));
+ //titleFileURL = decodeURIComponent(getURLParam('t'));
+ titleFileURL = decodeURIComponent(getURLParam('t')).replace(/_/g, " ");
  //console.log(Articles[titleFileURL]);
  indexFile = Object.keys(Articles).indexOf(titleFileURL);
  if (indexFile > 0)
@@ -49,7 +50,8 @@ function loadPrev() {
 
 function loadNext() {
  console.log("Next");
- titleFileURL = decodeURIComponent(getURLParam('t'));
+ //titleFileURL = decodeURIComponent(getURLParam('t'));
+ titleFileURL = decodeURIComponent(getURLParam('t')).replace(/_/g, " ");
  //console.log(Articles[titleFileURL]);
  indexFile = Object.keys(Articles).indexOf(titleFileURL);
  if (indexFile < ListFiles.length - 1)
@@ -78,12 +80,12 @@ function loadFile(titleFile, index, event) {
  document.querySelector('meta[property="og:title"]').setAttribute("content", titleFile);
  document.querySelector('meta[property="og:url"]').setAttribute("content", "https://medium-technica.github.io/aavarthi/?t=" + titleFile);
  console.log(document.querySelector('meta[property="og:title"]').getAttribute("content"));
- console.log(window.location.href);
- history.pushState({}, null, window.location.pathname + "?t=" + encodeURIComponent(titleFile));
+ history.pushState({}, null, window.location.pathname + "?t=" + encodeURIComponent(titleFile).replace(/%20/g, "_"));
+ console.log(decodeURIComponent((window.location.href).replace(/%20/g, "_")));
  if (index == 0) {
   $('.mdl-paging__prev').css("visibility", "hidden");
  } else {
-  document.querySelector('meta[property="og:description"]').setAttribute("content", "");
+  document.querySelector('meta[property="og:description"]').setAttribute("content", "ആവർത്തനങ്ങളുടെ സ്മൃതികൾ");
   $('.mdl-paging__prev').css("visibility", "visible");
  }
  if (index == ListFiles.length - 1) {
